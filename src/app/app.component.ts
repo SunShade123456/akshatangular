949a563d38d853cc967f5a7a2aeb130d4505cc73
SunShade123456 <akshat.anuj17@gmail.com>
2023-10-11 03:24:23 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'akshatangular';
+  title = 'Akshat';
+  x = 'Priyo';
+  y = 'angular';
+
+  pujo(){
+    alert('DurgaPujo Coming!');
+  }
+
+  uname = new FormControl("");
+  updateName()
+  {
+    this.uname.setValue("AKSHAT");
+  }
+
+  products=
+  [
+    {
+      brand:'Sony',
+      price:40000,
+      type:'LCD'
+    },
+    {
+      brand:'Samsung',
+      price:45000,
+      type:'OLED'
+    },
+    {
+      brand:'LG',
+      price:35000,
+      type:'LCD'
+    },
+    {
+      brand:'OnePlus',
+      price:50000,
+      type:'OLED'
+    },
+  ];
+
 }
